a9af1a0cefdc7fd91b0dcc2ef5e940eb97c496f2
Vesela Videva <vesela.videva@gmail.com>
2021-11-11 18:27:46 +0200

    Add base react app code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <p>COSMOS app in progress...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
