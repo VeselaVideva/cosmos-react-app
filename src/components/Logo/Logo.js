374044fc1545f8e682e32f083702c2100630905f
Vesela Videva <vesela.videva@gmail.com>
2021-11-12 14:34:12 +0200

    Turn logo to a home page link

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,6 +2,8 @@ import './Logo.css';
 
 export default function Logo() {
     return (
-        <img className='logo' src="logo.png" alt="Cosmos logo" />
+        <a className='logo' href="/">
+            <img src="logo.png" alt="Cosmos logo" />
+        </a>
     );
 }
