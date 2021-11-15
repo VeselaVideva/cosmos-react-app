f43cd19e3f0627aef0aa5fe3b6dd2ce631890ec4
Vesela Videva <vesela.videva@gmail.com>
2021-11-15 23:21:29 +0200

    Add simple Header routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import { useState } from 'react';
 import './App.css';
+import {} from './firebase/config';
+
+// Components
 import Background from './components/Background';
 import Header from './components/Header/Header';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 
 export default function App() {
+    const [page, setPage] = useState('/');
+
+    const navigationChangeHandler = (path) => {
+        setPage(path);
+    }
+
+    const router = (path) => {
+        let pathNames = path.split('/');
+
+        let rootPath = pathNames[1];
+        let argument = pathNames[2];
+
+        const routes = {
+            'login': <Login />,
+            'register': <Register />,
+        };
+
+        return routes[rootPath];
+    }
+
     return (
         <>
             <Background />
             <div className="App">
-                <Header />
+                <Header navigationChangeHandler={navigationChangeHandler} />
+                { router(page) || '' }
             </div>
         </>
     );
