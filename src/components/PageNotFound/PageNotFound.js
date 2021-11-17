5b2ddc6cf82401355bf7ba61a1253d9aab91c588
Vesela Videva <vesela.videva@gmail.com>
2021-11-17 23:17:55 +0200

    Add error page - 404 Page Not Found

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+import './PageNotFound.css';
+
+const PageNotFound = () => {
+    return (
+        <div className="error-page">
+            <h1 className="huge-text">404</h1>
+        </div>
+    );
+}
+
+export default PageNotFound;
