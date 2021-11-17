0a94f5ec511c926a2f3cbcfe59d2161ca39e5c1e
Vesela Videva <vesela.videva@gmail.com>
2021-11-17 10:35:21 +0200

    Add new Explore page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './normalize.css';
 import './App.css';
 import {} from './firebase/config';
 
@@ -7,6 +8,7 @@ import Background from './components/Background';
 import Header from './components/Header/Header';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import Explore from './components/Explore/Explore'; // page showing all destinations (planets)
 
 export default function App() {
     const [page, setPage] = useState('/');
@@ -19,11 +21,12 @@ export default function App() {
         let pathNames = path.split('/');
 
         let rootPath = pathNames[1];
-        let argument = pathNames[2];
+        let argument = pathNames[2]; // for each planet details
 
         const routes = {
             'login': <Login />,
             'register': <Register />,
+            'explore': <Explore />,
         };
 
         return routes[rootPath];
---
+++ src/components/Explore/Explore.js
@@ -0,0 +1,11 @@
+import './Explore.css';
+
+const Explore = () => {
+    return (
+        <div className="explore">
+            <p>Write service to show all planets as destinations...</p>
+        </div>
+    );
+}
+
+export default Explore;
