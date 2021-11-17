44a5b5eb83f6a461941e9940a8ea8b992c58998d
Vesela Videva <vesela.videva@gmail.com>
2021-11-17 23:18:47 +0200

    Add Home page component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <p>Write animated text...</p>
+        </div>
+    );
+}
+
+export default Home;
