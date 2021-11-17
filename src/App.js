67036dc9a215ce68011525ce0aaee77b1a0b9657
Vesela Videva <vesela.videva@gmail.com>
2021-11-17 23:17:23 +0200

    Add simple routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './normalize.css';
 import './App.css';
 import {} from './firebase/config';
@@ -6,38 +6,29 @@ import {} from './firebase/config';
 // Components
 import Background from './components/Background';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import Explore from './components/Explore/Explore'; // page showing all destinations (planets)
+import PageNotFound from './components/PageNotFound/PageNotFound';
 
 export default function App() {
-    const [page, setPage] = useState('/');
-
-    const navigationChangeHandler = (path) => {
-        setPage(path);
-    }
-
-    const router = (path) => {
-        let pathNames = path.split('/');
-
-        let rootPath = pathNames[1];
-        let argument = pathNames[2]; // for each planet details
-
-        const routes = {
-            'login': <Login />,
-            'register': <Register />,
-            'explore': <Explore />,
-        };
-
-        return routes[rootPath];
-    }
-
     return (
         <>
             <Background />
             <div className="App">
-                <Header navigationChangeHandler={navigationChangeHandler} />
-                { router(page) || '' }
+                <Header />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/explore" exact component={Explore} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                    <Route path="/logout" render={(props) => {
+                        console.log('Logged Out!!!');
+                        return <Redirect to="/" />
+                    }} />
+                    <Route path="/*" component={PageNotFound} />
+                </Switch>
             </div>
         </>
     );
