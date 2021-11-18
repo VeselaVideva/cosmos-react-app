471ecf5cd8825650fe93740dcb40acfe44402a9a
Vesela Videva <vesela.videva@gmail.com>
2021-11-18 13:03:34 +0200

    Update Auth forms - add history, add routing

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,14 +1,26 @@
 import './Auth.css';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({
+    history
+}) => {
+    let historyHook = useHistory();
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        // TODO: Login
+        historyHook.push('/');
+    };
+
     return (
         <div className="login">
-            <form>
+            <form onSubmit={onFormSubmit}>
                 <h1>Sign In</h1>
                 <input type="email" name="email" placeholder="Email" required/>
                 <input type="password" name="password" placeholder="Password" required/>
                 <input className="submit" type="submit" value="Sign in"/>
-                <p>Don't have an account? <a className="internal-link" href="/register">Sign Up</a></p>
+                <p>Don't have an account? <Link className="internal-link" to="/register">Sign Up</Link></p>
             </form>
         </div>
     );
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,14 +1,27 @@
 import './Auth.css';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
-const Register = () => {
+const Register = ({
+    history
+}) => {
+    let historyHook = useHistory();
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        // TODO: Register
+        historyHook.push('/login');
+    };
+
     return (
         <div className="register">
-            <form>
+            <form onSubmit={onFormSubmit}>
                 <h1>Sign Up</h1>
                 <input type="email" name="email" placeholder="Email" required />
                 <input type="password" name="password" placeholder="Password" required />
+                <input type="password" name="confirm-password" placeholder="Confirm Password" required />
                 <input className="submit" type="submit" value="Sign up"/>
-                <p>Already have an account? <a className="internal-link" href="/login">Sign In</a></p>
+                <p>Already have an account? <Link className="internal-link" to="/login">Sign In</Link></p>
             </form>
         </div>
     );
