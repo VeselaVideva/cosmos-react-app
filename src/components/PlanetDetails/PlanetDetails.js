1a1288e274acd5589275abb951a0db45904fc04a
Vesela Videva <vesela.videva@gmail.com>
2021-11-18 18:40:09 +0200

    Add Details component

---
+++ src/components/PlanetDetails/PlanetDetails.js
@@ -0,0 +1,33 @@
+import './PlanetDetails.css';
+import Loading from '../Loading/Loading';
+import { useEffect, useState, Suspense } from 'react';
+import * as planetService from '../../services/planetService';
+import DetailsCard from '../DetailsCard/DetailsCard';
+
+const PlanetDetails = ({
+    match,
+    planetName,
+}) => {
+    const [planet, setPlanet] = useState([]);
+
+    useEffect(() => {
+        planetService.getOne(match.params.planetName)
+            .then(result => {
+                setPlanet(result);
+            })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <div className="explore">
+            <Suspense fallback={<Loading />}>
+                {planet !== undefined
+                    ? planet.filter(x => x !== undefined).map((x) => <DetailsCard key={x.name} planet={x} />)
+                    : <Loading />
+                }
+            </Suspense>
+        </div>
+    );
+}
+
+export default PlanetDetails;
--- src/services/planetService.js
+++ src/services/planetService.js
@@ -5,8 +5,17 @@ const db = getFirestore();
 // Get all planets from my database (collection data)
 export async function getAll() {
     const snapshot = await getDocs(collection(db, "planets"));
-
     return snapshot.docs.map((doc) => {
         return doc.data();
+    });
+}
+
+// Get only one planet
+export async function getOne(planetName) {
+    const snapshot = await getDocs(collection(db, "planets"));
+    return snapshot.docs.map((doc) => {
+        if (doc.data().name === planetName) {
+            return doc.data();
+        }
     });
 }
