248416db96c91452f8c9f15483a51fc095e52b45
Vesela Videva <vesela.videva@gmail.com>
2021-11-18 13:04:47 +0200

    Add PlanetCard component

---
+++ src/components/Card/PlanetCard.js
@@ -0,0 +1,16 @@
+import './PlanetCard.css';
+import { Link } from 'react-router-dom';
+
+const PlanetCard = ({
+    planet,
+}) => {
+    return (
+        <div className="planet-card" key={planet.name}>
+            <img src={planet.image} alt={planet.name} />
+            <h2>{planet.name}</h2>
+            <Link to={`/explore/${planet.name}`} className="card-btn">Details</Link>
+        </div>
+    );
+}
+
+export default PlanetCard;
