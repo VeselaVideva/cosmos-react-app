a5c34cba026f6ddfa797bed190d14cf103677728
Vesela Videva <vesela.videva@gmail.com>
2021-11-18 13:05:29 +0200

    Update Explore page - add loader, add all planets

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -1,9 +1,29 @@
 import './Explore.css';
+import Loading from '../Loading/Loading';
+import { useEffect, useState, Suspense } from 'react';
+import * as planetService from '../../services/planetService';
+import PlanetCard from '../Card/PlanetCard';
 
 const Explore = () => {
+    const [planets, setPlanets] = useState([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            planetService.getAll()
+                .then(result => {
+                    setPlanets(result);
+                })
+        }, 1000);
+    }, []);
+
     return (
         <div className="explore">
-            <p>Write service to show all planets as destinations...</p>
+            <Suspense fallback={<Loading />}>
+                {planets.length > 0
+                    ? planets.map(x => <PlanetCard key={x.name} planet={x} />)
+                    : <Loading />
+                }
+            </Suspense>
         </div>
     );
 }
