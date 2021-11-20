786fd51e20a99d09bde16f1e0bbbb8dd198c3137
Vesela Videva <vesela.videva@gmail.com>
2021-11-20 22:22:27 +0200

    Update PageNotFound

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,9 +1,13 @@
 import './PageNotFound.css';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
     return (
         <div className="error-page">
             <h1 className="huge-text">404</h1>
+            <h1 className="large-text">Page Not Found</h1>
+            <p>Ooops, sorry but you've entered into a Black Hole!</p>
+            <p>The gravity is so strong that it can kills you, you must act fast and go <Link to="/" className="internal-link">Home</Link></p>
         </div>
     );
 }
