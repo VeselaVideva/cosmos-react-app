65160b7482aa159b9d1dbdfba6bef8f00826a750
Vesela Videva <vesela.videva@gmail.com>
2021-11-25 19:26:34 +0200

    Add Populate page - for adding a new species

---
+++ src/components/Populate/Populate.js
@@ -0,0 +1,11 @@
+import './Populate.css';
+
+const Populate = () => {
+    return (
+        <div className="populate">
+            <h1>Add new species form</h1>
+        </div>
+    )
+}
+
+export default Populate;
