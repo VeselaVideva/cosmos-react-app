93689053034c6e80a8c6cf750bdb45037fdd0d2c
Vesela Videva <vesela.videva@gmail.com>
2021-11-25 09:46:21 +0200

    Fix minor issue

--- src/utils/getUsername.js
+++ src/utils/getUsername.js
@@ -1,7 +1,6 @@
 export function getUsername(user) {
     if (user) {
         return user.email.charAt(0).toUpperCase() + user.email.slice(1).split('@')[0];
-    } else {
-        return undefined;
     }
+    return undefined;
 }
