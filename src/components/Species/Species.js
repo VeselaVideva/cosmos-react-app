500ff421b5c1d499a3e7c36d33da255cd9ba44aa
Vesela Videva <vesela.videva@gmail.com>
2021-11-25 19:26:10 +0200

    Add 'All species' page

---
+++ src/components/Species/Species.js
@@ -0,0 +1,11 @@
+import './Species.css';
+
+const Species = () => {
+    return (
+        <div className="species">
+            <h1>Get all inhabitants from db</h1>
+        </div>
+    )
+}
+
+export default Species;
