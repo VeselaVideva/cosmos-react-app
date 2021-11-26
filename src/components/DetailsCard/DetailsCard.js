1b88405e479e03bc6a86cce7c6ee565a8ead4364
Vesela Videva <vesela.videva@gmail.com>
2021-11-26 15:17:50 +0200

    Refactor planet card and add iframe with NASA interactive info

--- src/components/DetailsCard/DetailsCard.js
+++ src/components/DetailsCard/DetailsCard.js
@@ -1,14 +1,18 @@
 import './DetailsCard.css';
+import Loading from '../Loading/Loading';
+import { Suspense } from 'react';
 
 const DetailsCard = ({
     planet
 }) => {
     return (
         <div className="planet-card-details" key={planet.name}>
-            <img src={planet.image} alt={planet.name}/>
-            <h2><span>Name:</span> {planet.name}</h2>
             <div className="card-grid">
                 <div>
+                    <img src={planet.image} alt={planet.name}/>
+                </div>
+                <div>
+                    <h2><span>Name:</span> {planet.name}</h2>
                     <p><span>Mass:</span> {planet.mass} x 10<sup>24</sup>kg</p>
                     <p><span>Diameter:</span> {planet.diameter}km</p>
                     <p><span>Gravity:</span> {planet.gravity}m/s<sup>2</sup></p>
@@ -19,6 +23,16 @@ const DetailsCard = ({
                     <p><span>Description:</span> {planet.description}</p>
                 </div>
             </div>
+            <Suspense fallback={<Loading />}>
+                <iframe
+                    width="100%"
+                    height="700px"
+                    src={`https://solarsystem.nasa.gov/planets/${planet.name.toLowerCase()}/overview/`}
+                    frameBorder="0"
+                    scrolling="no"
+                    allowFullScreen>
+                </iframe>
+            </Suspense>
         </div>
     );
 }
