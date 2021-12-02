fcbbd55a2c7b14fc6d676764248d8f0ff0773ca2
Vesela Videva <vesela.videva@gmail.com>
2021-12-02 14:52:03 +0200

    Add Update functionality

---
+++ src/components/Populate/Update.js
@@ -0,0 +1,68 @@
+import './Populate.css';
+import { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { getOne, updateOne } from '../../services/speciesService';
+
+const Update = ({
+    history
+}) => {
+    let historyHook = useHistory();
+
+    const match = useParams();
+    const speciesId = match.speciesId;
+
+    const [species, setSpecies] = useState([]);
+
+    useEffect(() => {
+        getOne(speciesId)
+            .then(result => {
+                setSpecies(result);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }, [speciesId]);
+
+    const onFormSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const species = formData.get('species');
+        const lifespan = formData.get('lifespan');
+        const image = formData.get('image');
+        const description = formData.get('description');
+
+        if (species === '' || lifespan === '' || image === '' || description === '') {
+            throw new Error(`All fields are required!`);
+        }
+
+        await updateOne(speciesId, { species, lifespan, image, description })
+            .then(() => {
+                e.target.reset();
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+
+        historyHook.push('/all-species');
+    }
+
+    return (
+        <div className="populate">
+            <form onSubmit={onFormSubmit}>
+                <h1>Update <span className="accent">{species.species}</span> info</h1>
+                <label htmlFor="species">Species name:</label>
+                <input type="text" name="species" placeholder={species.species} required />
+                <label htmlFor="lifespan">Lifespan:</label>
+                <input type="number" name="lifespan" placeholder={species.lifespan} required />
+                <label htmlFor="image">Image:</label>
+                <input type="url" name="image" placeholder={species.image} required />
+                <label htmlFor="description">Description:</label>
+                <input type="text" name="description" placeholder={species.description} required />
+                <input className="submit" type="submit" value="Update species"/>
+            </form>
+        </div>
+    )
+}
+
+export default Update;
--- src/services/speciesService.js
+++ src/services/speciesService.js
@@ -1,13 +1,51 @@
 import {
     getFirestore,
+    doc,
     collection,
+    getDoc,
+    getDocs,
     addDoc,
-    getDocs
+    updateDoc
 } from "firebase/firestore";
 
 const db = getFirestore();
 
-// Get all species from database
+// Get all species
+export async function getAllSpecies() {
+    const snapshot = await getDocs(collection(db, "species"));
+    return snapshot.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
+    });
+}
+
+// Get only one species
+export async function getOne(speciesId) {
+    const docRef = doc(db, "species", speciesId);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        return docSnap.data();
+    } else {
+        throw new Error("No such species!");
+    }
+}
+
+// Get all species for a specific planet
+export async function getPlanetSpecies(planetName) {
+    const snapshot = await getDocs(collection(db, "species"));
+    return snapshot.docs.filter(doc => doc.data().planet === planetName).map((doc) => {
+        return { ...doc.data(), id: doc.id };
+    });
+}
+
+// Get all species added by specific user
+export async function getUserSpecies(userEmail) {
+    const snapshot = await getDocs(collection(db, "species"));
+    return snapshot.docs.filter(doc => doc.data().owner === userEmail).map((doc) => {
+        return { ...doc.data(), id: doc.id };
+    });
+}
+
+// Add new species
 export async function addNew({ species, lifespan, image, description, planet, owner }) {
     const docRef = await addDoc(collection(db, "species"), {
         species,
@@ -20,26 +58,9 @@ export async function addNew({ species, lifespan, image, description, planet, ow
     return docRef;
 }
 
-// Get all species from database
-export async function getAllSpecies() {
-    const snapshot = await getDocs(collection(db, "species"));
-    return snapshot.docs.map((doc) => {
-        return { ...doc.data(), id: doc.id };
-    });
-}
-
-// Get species for a specific planet
-export async function getPlanetSpecies(planetName) {
-    const snapshot = await getDocs(collection(db, "species"));
-    return snapshot.docs.filter(doc => doc.data().planet === planetName).map((doc) => {
-        return { ...doc.data(), id: doc.id };
-    });
-}
-
-// Get species for a specific user
-export async function getUserSpecies(userEmail) {
-    const snapshot = await getDocs(collection(db, "species"));
-    return snapshot.docs.filter(doc => doc.data().owner === userEmail).map((doc) => {
-        return { ...doc.data(), id: doc.id };
-    });
+// Edit existing species
+export async function updateOne(speciesId, { species, lifespan, image, description }) {
+    const docRef = doc(db, "species", speciesId);
+    const payload = { species, lifespan, image, description };
+    await updateDoc(docRef, payload);
 }
