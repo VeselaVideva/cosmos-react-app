a9ed3ecce67a3af6c8ceb6faec897625dab2718e
Vesela Videva <vesela.videva@gmail.com>
2021-12-05 00:55:21 +0200

    Add AuthProvider, wrap whole App, refactor, wrap App with BrowserRouter
    Only in App component I'm using the useAuth hook, because it is the only way to get the current user outside of the context and to pass the user state to the Router.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
 import './normalize.css';
 import './App.css';
+
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
 import {} from './firebase/config';
+
+import { AuthProvider } from './contexts/AuthContext';
+
 import { useAuth } from './services/authService';
 
 // Components
@@ -12,52 +17,51 @@ import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import Explore from './components/Explore/Explore'; // page showing all planets
 import PlanetDetails from './components/PlanetDetails/PlanetDetails';
-import Populate from './components/Populate/Populate';
+import Populate from './components/Populate/Populate'; // create
 import Update from './components/Populate/Update';
 import Profile from './components/Profile/Profile';
-import Species from './components/Species/Species';
-import PageNotFound from './components/PageNotFound/PageNotFound';
-import AuthRouteGuard from './components/Auth/AuthRouteGuard'; // 404 page
+import Species from './components/Species/Species'; // catalog
+import PageNotFound from './components/PageNotFound/PageNotFound'; // 404 page
+import AuthRouteGuard from './components/Auth/AuthRouteGuard';
 import Ownership from './components/Ownership/Ownership';
 
-export default function App() {
-    let isAuthenticated = false;
 
+export default function App() {
     const currentUser = useAuth();
 
-    if (currentUser !== null) {
-        isAuthenticated = true;
-    }
+    const isAuth = Boolean(currentUser !== null);
 
     return (
-        <>
-            <Background />
-            <div className="App">
-                <Header />
-                <Switch>
-                    <Route path="/" exact component={ Home } />
-                    <Route path="/explore" exact component={ Explore } />
-                    <Route path="/explore/:planetName" component={ PlanetDetails } />
-                    <Route path="/all-species" exact component={ Species } />
-                    <Route path="/all-species/:speciesId/edit">
-                        { isAuthenticated ? <Update /> : <Redirect to="/login" /> }
-                    </Route>
-                    <Route path="/populate">
-                        { isAuthenticated ? <Populate /> : <Redirect to="/login" /> }
-                    </Route>
-                    <Route path="/profile">
-                        { isAuthenticated ? <Profile /> : <Redirect to="/login" /> }
-                    </Route>
-                    <Route path="/login">
-                        { !isAuthenticated ? <Login /> : <AuthRouteGuard /> }
-                    </Route>
-                    <Route path="/register">
-                        { !isAuthenticated ? <Register /> : <AuthRouteGuard /> }
-                    </Route>
-                    <Route path="/*" component={ PageNotFound } />
-                </Switch>
-                <Ownership />
-            </div>
-        </>
+        <AuthProvider>
+            <BrowserRouter>
+                <Background />
+                <div className="App">
+                    <Header/>
+                    <Switch>
+                        <Route path="/" exact component={ Home } />
+                        <Route path="/explore" exact component={ Explore } />
+                        <Route path="/explore/:planetName" component={ PlanetDetails } />
+                        <Route path="/all-species" exact component={ Species } />
+                        <Route path="/all-species/:speciesId/edit">
+                            { isAuth ? <Update /> : <Redirect to="/login" /> }
+                        </Route>
+                        <Route path="/populate">
+                            { isAuth ? <Populate /> : <Redirect to="/login" /> }
+                        </Route>
+                        <Route path="/profile">
+                            { isAuth ? <Profile /> : <Redirect to="/login" /> }
+                        </Route>
+                        <Route path="/login">
+                            { !isAuth ? <Login /> : <AuthRouteGuard /> }
+                        </Route>
+                        <Route path="/register">
+                            { !isAuth ? <Register /> : <AuthRouteGuard /> }
+                        </Route>
+                        <Route path="/*" component={ PageNotFound } />
+                    </Switch>
+                    <Ownership />
+                </div>
+            </BrowserRouter>
+        </AuthProvider>
     );
 }
