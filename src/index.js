4d49fd70a654a96f5a09eb27cf5a228bd8b54b67
Vesela Videva <vesela.videva@gmail.com>
2021-12-05 00:44:45 +0200

    Move Router from index to App

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-      <Router>
-          <App />
-      </Router>
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
