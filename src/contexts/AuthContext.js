a81c4a6c054c0bbe9b5f272eb4e667a5439aace3
Vesela Videva <vesela.videva@gmail.com>
2021-12-05 00:46:47 +0200

    Create AuthContext and AuthProvider
    I'm using the ContextAPI for wrapping the whole App with Context Provider and passing the current user state.

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,24 @@
+import { createContext, useState, useEffect } from 'react';
+
+import {} from '../firebase/config';
+
+import { onAuthStateChanged } from "firebase/auth";
+
+import { auth } from '../services/authService';
+
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        return onAuthStateChanged(auth, user => setCurrentUser(user));
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ currentUser }}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
