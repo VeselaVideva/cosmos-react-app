e347c47754b37af885d39b3d6ef5fcfb5e12c67f
Vesela Videva <vesela.videva@gmail.com>
2021-12-08 11:36:40 +0200

    Create Delete page with confirmation option

---
+++ src/components/Populate/Delete.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import './Populate.css';
+
+import { getOne, deleteOne } from '../../services/speciesService';
+
+
+const Delete = ({
+    history
+}) => {
+    let historyHook = useHistory();
+
+    const match = useParams();
+    const speciesId = match.speciesId;
+
+    const [species, setSpecies] = useState([]);
+    const [error, setError] = useState([]);
+
+    useEffect(() => {
+        getOne(speciesId)
+            .then(result => {
+                setSpecies(result);
+            })
+            .catch((err) => {
+                return setError(err.message);
+            })
+    }, [speciesId]);
+
+    const deleteHandler = () => {
+        deleteOne(speciesId)
+            .then(result => {
+                setSpecies(result);
+                historyHook.push('/all-species');
+                return species;
+            })
+            .catch((err) => {
+                return setError(err.message);
+            })
+    }
+
+    return (
+        <div className="populate">
+            { error.length > 0 ? <div className="error-box">{ error }</div> : '' }
+            <div className="flex">
+                <h2>Are you sure you want to destroy all <span className="accent">{species?.species}</span>'s?</h2>
+                <h2><span>&#9888;</span> This action is irreversible!</h2>
+                <img className="crying-alien" src="https://pyxis.nymag.com/v1/imgs/fc7/90e/5120d18b22947134a593434c91c7e656c0-28-crying-alien.rsocial.w1200.jpg" alt="Sad alien" />
+                <div className="buttons-block">
+                    <div className="confirm-btn alert-btn" onClick={ deleteHandler }>Confirm</div>
+                    <div className="confirm-btn" onClick={ historyHook.goBack }>Go Back</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Delete;
