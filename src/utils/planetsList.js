f68c48eeb4ffd36bb09115034f207ce4cfb7763d
Vesela Videva <vesela.videva@gmail.com>
2021-12-08 17:49:42 +0200

    Create constant for the planets array

---
+++ src/utils/planetsList.js
@@ -0,0 +1,11 @@
+export const planets = [
+    { value: 'Mercury', name: 'Mercury' },
+    { value: 'Venus', name: 'Venus' },
+    { value: 'Earth', name: 'Earth' },
+    { value: 'Mars', name: 'Mars' },
+    { value: 'Jupiter', name: 'Jupiter' },
+    { value: 'Saturn', name: 'Saturn' },
+    { value: 'Uranus', name: 'Uranus' },
+    { value: 'Neptune', name: 'Neptune' },
+    { value: 'Pluto', name: 'Pluto' }
+];
