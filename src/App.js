1954bfaaed18c31378bc4ac5cdf99379d02cd760
Vesela Videva <vesela.videva@gmail.com>
2021-12-12 22:19:19 +0200

    Implement code-splitting in App component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './normalize.css';
 import './App.css';
@@ -9,22 +10,23 @@ import { NotificationProvider } from './contexts/NotificationContext';
 import { useAuth } from './hooks/useAuth';
 
 // Components
-import Background from './components/Background';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
-import Explore from './components/Explore/Explore'; // page showing all planets
-import PlanetDetails from './components/PlanetDetails/PlanetDetails';
-import Populate from './components/Populate/Populate'; // create
-import Update from './components/Populate/Update';
-import Delete from './components/Populate/Delete';
-import Profile from './components/Profile/Profile';
-import Species from './components/Species/Species'; // catalog
-import PageNotFound from './components/PageNotFound/PageNotFound'; // 404 page
-import AuthRouteGuard from './components/Auth/AuthRouteGuard';
-import Notification from './components/Notification/Notification';
-import Ownership from './components/Ownership/Ownership';
+import Loading from './components/Loading/Loading'; // fallback UI
+const Background = lazy(() => import('./components/Background'));
+const Header = lazy(() => import('./components/Header/Header'));
+const Home = lazy(() => import('./components/Home/Home'));
+const Login = lazy(() => import('./components/Auth/Login'));
+const Register = lazy(() => import('./components/Auth/Register'));
+const Explore = lazy(() => import('./components/Explore/Explore')); // page showing all planets
+const PlanetDetails = lazy(() => import('./components/PlanetDetails/PlanetDetails'));
+const Populate = lazy(() => import('./components/Populate/Populate')); // create
+const Update = lazy(() => import('./components/Populate/Update'));
+const Delete = lazy(() => import('./components/Populate/Delete'));
+const Profile = lazy(() => import('./components/Profile/Profile'));
+const Species = lazy(() => import('./components/Species/Species')); // catalog
+const PageNotFound = lazy(() => import('./components/PageNotFound/PageNotFound')); // 404 page
+const AuthRouteGuard = lazy(() => import('./components/Auth/AuthRouteGuard'));
+const Notification = lazy(() => import('./components/Notification/Notification'));
+const Ownership = lazy(() => import('./components/Ownership/Ownership'));
 
 
 export default function App() {
@@ -36,37 +38,39 @@ export default function App() {
         <AuthProvider>
             <NotificationProvider>
                 <BrowserRouter>
-                    <Background />
-                    <div className="App">
-                        <Header/>
-                        <Notification />
-                        <Switch>
-                            <Route path="/" exact component={ Home } />
-                            <Route path="/explore" exact component={ Explore } />
-                            <Route path="/explore/:planetName" component={ PlanetDetails } />
-                            <Route path="/all-species" exact component={ Species } />
-                            <Route path="/all-species/:speciesId/edit">
-                                { isAuth ? <Update /> : <Redirect to="/login" /> }
-                            </Route>
-                            <Route path="/all-species/:speciesId/delete">
-                                { isAuth ? <Delete /> : <Redirect to="/login" /> }
-                            </Route>
-                            <Route path="/populate">
-                                { isAuth ? <Populate /> : <Redirect to="/login" /> }
-                            </Route>
-                            <Route path="/profile">
-                                { isAuth ? <Profile /> : <Redirect to="/login" /> }
-                            </Route>
-                            <Route path="/login">
-                                { !isAuth ? <Login /> : <AuthRouteGuard /> }
-                            </Route>
-                            <Route path="/register">
-                                { !isAuth ? <Register /> : <AuthRouteGuard /> }
-                            </Route>
-                            <Route path="/*" component={ PageNotFound } />
-                        </Switch>
-                        <Ownership />
-                    </div>
+                    <Suspense fallback={<Loading />}>
+                        <Background />
+                        <div className="App">
+                            <Header/>
+                            <Notification />
+                            <Switch>
+                                <Route path="/" exact component={ Home } />
+                                <Route path="/explore" exact component={ Explore } />
+                                <Route path="/explore/:planetName" component={ PlanetDetails } />
+                                <Route path="/all-species" exact component={ Species } />
+                                <Route path="/all-species/:speciesId/edit">
+                                    { isAuth ? <Update /> : <Redirect to="/login" /> }
+                                </Route>
+                                <Route path="/all-species/:speciesId/delete">
+                                    { isAuth ? <Delete /> : <Redirect to="/login" /> }
+                                </Route>
+                                <Route path="/populate">
+                                    { isAuth ? <Populate /> : <Redirect to="/login" /> }
+                                </Route>
+                                <Route path="/profile">
+                                    { isAuth ? <Profile /> : <Redirect to="/login" /> }
+                                </Route>
+                                <Route path="/login">
+                                    { !isAuth ? <Login /> : <AuthRouteGuard /> }
+                                </Route>
+                                <Route path="/register">
+                                    { !isAuth ? <Register /> : <AuthRouteGuard /> }
+                                </Route>
+                                <Route path="/*" component={ PageNotFound } />
+                            </Switch>
+                            <Ownership />
+                        </div>
+                    </Suspense>
                 </BrowserRouter>
             </NotificationProvider>
         </AuthProvider>
