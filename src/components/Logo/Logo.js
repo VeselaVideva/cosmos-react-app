1ae90d3873ce9011662f4d05fe1888a820f36afc
Vesela Videva <vesela.videva@gmail.com>
2021-12-13 12:08:34 +0200

    Image and video size optimization

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import './Logo.css';
 const Logo = () => {
     return (
         <NavLink className='logo' to="/">
-            <img src="/logo.png" alt="Cosmos logo" />
+            <img src="/logo.png" alt="Cosmos logo" height="92px" width="152px" />
         </NavLink>
     );
 }
